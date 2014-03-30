c13c402a6fbd30d2e1066055bc3f0171df83247a
Jared Faris <jfaris@gmail.com>
2014-03-30 18:52:55 -0400

    Finished implementing the useless machine

--- MonoBrickInitialTest/Program.cs
+++ MonoBrickInitialTest/Program.cs
@@ -19,8 +19,8 @@ namespace MotorExample
 			// pull the arm back slightly to the starting position
 			ResetArmLocation (motor);
 
-
-			Task.Factory.StartNew(() => MainLoop());
+			// a loop needs to run to watch the arm and reset the switch if someone is rude enough to push it
+			Task.Factory.StartNew(() => MainLoop(motor));
 
 			// Setup the down button to terminate the app
 			ButtonEvents buts = new ButtonEvents ();  
@@ -30,27 +30,6 @@ namespace MotorExample
 			};  
 
 			terminateProgram.WaitOne();
-
-//			Motor motor = new Motor (MotorPort.OutA);
-//			motor.On(2);
-//			motor.Off();
-//			LcdConsole.WriteLine ("Running single motor test");
-//			LcdConsole.WriteLine ("Reset motor tacho");
-//			motor.ResetTacho ();
-//			LcdConsole.WriteLine ("Running forward with 20");
-//			motor.On(20);
-//			System.Threading.Thread.Sleep(2500);
-//			LcdConsole.WriteLine ("Printing motor speed: " + motor.GetSpeed());
-//			System.Threading.Thread.Sleep(2500);
-//			motor.Off();		
-//			System.Threading.Thread.Sleep(2500);
-//
-//			LcdConsole.WriteLine ("Move to zero");
-//			motor.MoveTo(40, 0, true);
-//			LcdConsole.WriteLine("Motor at position: " + motor.GetTachoCount());
-//			System.Threading.Thread.Sleep(2000);
-//
-//			LcdConsole.WriteLine ("Done executing motor test");
 		}
 
 		/// <summary>
@@ -65,6 +44,9 @@ namespace MotorExample
 			// to fast then it will still be zero
 			System.Threading.Thread.Sleep (200);
 
+			// we'll be using the tachometer to track rotations so lets reset it
+			motor.ResetTacho ();
+
 			while (motor.GetSpeed() > 0) {
 				// just keeps us from moving on until the arm is fully reset
 				// hacky I know
@@ -72,32 +54,50 @@ namespace MotorExample
 
 			LcdConsole.WriteLine ("Arm reset");
 			motor.Off();
-//			motor.On (10);
-//
-//			System.Threading.Thread.Sleep (200);
-//
-//			while (motor.GetSpeed () > 0) {
-//				System.Threading.Thread.Sleep (50);
-//			}
-//			motor.Off ();
 		}
 
-		private static void MainLoop() {
+		/// <summary>
+		/// Repeatedly checks the state of the switch and resets it necessary
+		/// </summary>
+		private static void MainLoop(Motor motor) {
 			var touchSensor = new TouchSensor(SensorPort.In1);
 
 			while (true) {
-				LcdConsole.WriteLine ("Looping");
 
-				// if the touch sensor is pressed, display "off"
-				if (touchSensor.IsPressed()) {
-					LcdConsole.WriteLine ("Off");
-				} else {
-					LcdConsole.WriteLine ("On");
+				// there are a few states that the switch and the arm can be in
+				// our default is the arm down, and the switch off
+				// if someone flips the switch, we'll move our arm up until it completes its motion
+				// at that point, the switch will be off again and we'll move the arm back
+
+				// Our "switch" is off when the touch sensor is pressed (inverse) 
+				var switchStatus = !touchSensor.IsPressed ();
+
+				// When our switch is on, we need to turn it back off
+				if (switchStatus) {
+					// if the motor hasn't moved backwards to push the switch off, we'll do that
+					if (motor.GetTachoCount () > -120) {
+						LcdConsole.WriteLine ("(On) Extending arm");
+						motor.On (-10);
+					} else { // if it's moved all the way back, stop pushing it
+						LcdConsole.WriteLine ("(On) Arm fully extended");
+						motor.Off ();
+					}
+
+
+				} else { // When it's off, we're happy
+					// if the arm isn't fully retracted, it's time to put it back to zero
+					if (motor.GetTachoCount () < -20) {
+						LcdConsole.WriteLine ("(Off) Reset the arm");
+						motor.On (10);
+					} else { // if it is, we're done
+						LcdConsole.WriteLine ("(Off) Arm fully reset");
+						motor.Off ();
+					}
 				}
 
 				// else display "on"
 
-				System.Threading.Thread.Sleep (1000);
+//				System.Threading.Thread.Sleep (1000);
 			}
 		}
 	}
